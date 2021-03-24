5d08a7580453ee792ecc5e22603964320aca2b39
KZEN17 <zack.kargeen@gmail.com>
2021-03-24 18:58:07 +0700

    sgn in and register complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,60 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import CartScreen from './screens/CartScreen';
 import HomeScreen from './screens/HomeScreen';
+import { signout } from './actions/userActions';
 import ProductScreen from './screens/ProductScreen';
+import SigninScreen from './screens/SigninScreen';
+import RegisterScreen from './screens/RegisterScreen';
 function App() {
+	const cart = useSelector((state) => state.cart);
+	const { cartItems } = cart;
+	const userSignin = useSelector((state) => state.userSignin);
+	const { userInfo } = userSignin;
+	const dispatch = useDispatch();
+	const signoutHandler = () => {
+		dispatch(signout());
+	};
 	return (
 		<BrowserRouter>
 			<div className="grid-container">
 				<header className="row">
 					<div>
-						<a className="brand" href="/">
-							amazonia
-						</a>
+						<Link className="brand" to="/">
+							KZEN Tech Store
+						</Link>
 					</div>
 					<div>
-						<a href="/cart">My Cart</a>
-						<a href="/signin">Sign In</a>
+						<Link to="/cart">
+							My Cart
+							{cartItems.length > 0 && (
+								<span className="badge">{cartItems.length}</span>
+							)}
+						</Link>
+						{userInfo ? (
+							<div className="dropdown">
+								<Link to="#">
+									{userInfo.name} <i className="fa fa-caret-down"></i>{' '}
+								</Link>
+								<ul className="dropdown-content">
+									<li>
+										<Link to="#signout" onClick={signoutHandler}>
+											Sign Out
+										</Link>
+									</li>
+								</ul>
+							</div>
+						) : (
+							<Link to="/signin">Sign In</Link>
+						)}
 					</div>
 				</header>
 				<main>
-					<Route path="/product/:id" component={ProductScreen} exact></Route>
+					<Route path="/cart/:id?" component={CartScreen}></Route>
+					<Route path="/product/:id" component={ProductScreen}></Route>
+					<Route path="/signin" component={SigninScreen}></Route>
+					<Route path="/register" component={RegisterScreen}></Route>
 					<Route path="/" component={HomeScreen} exact></Route>
 				</main>
 				<footer className="row center">All rights reserved</footer>
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,14 +1,29 @@
-import data from './data';
 import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
 import thunk from 'redux-thunk';
 import {
 	productDetailsReducer,
 	productListReducer,
 } from './reducers/productReducers';
-const initialState = {};
+import { cartReducer } from './reducers/cartReucers';
+import { userRegisterReducer, userSigninReducer } from './reducers/userReducer';
+const initialState = {
+	userSignin: {
+		userInfo: localStorage.getItem('userInfo')
+			? JSON.parse(localStorage.getItem('userInfo'))
+			: null,
+	},
+	cart: {
+		cartItems: localStorage.getItem('cartItems')
+			? JSON.parse(localStorage.getItem('cartItems'))
+			: [],
+	},
+};
 const reducer = combineReducers({
 	productList: productListReducer,
 	productDetails: productDetailsReducer,
+	cart: cartReducer,
+	userSignin: userSigninReducer,
+	userRegister: userRegisterReducer,
 });
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
