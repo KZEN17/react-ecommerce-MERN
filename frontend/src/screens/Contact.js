56d173122454baa57db12bfc21cbd09d990724f6
KZEN17 <zack.kargeen@gmail.com>
2021-03-29 17:24:00 +0700

    favicon fixed

--- frontend/src/screens/Contact.js
+++ frontend/src/screens/Contact.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import emailjs from 'emailjs-com';
 
-// import './ContactUs.css';
-
 export default function ContactUs() {
 	function sendEmail(e) {
 		e.preventDefault();
